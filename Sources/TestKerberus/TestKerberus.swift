05395af2524a5ac9ebd84215c8bb7e5311bd0f8c
Clément VANDENDAELEN <cv@icure.com>
2024-10-03 13:45:08 +0200

    fix: Fixed static func modifier

--- Sources/TestKerberus/TestKerberus.swift
+++ Sources/TestKerberus/TestKerberus.swift
@@ -1,11 +1,11 @@
 import Kerberus
 
 public class Kerberus {
-    static func resolveChallenge(config: Challenge, serializedInput: String, onProgress: @escaping (Double) -> Void) async throws -> Solution {
+    public static func resolveChallenge(config: Challenge, serializedInput: String, onProgress: @escaping (Double) -> Void) async throws -> Solution {
         return try await KerberusKt.resolveChallenge(config: config, serializedInput: serializedInput) { progress in onProgress(progress.doubleValue) }
     }
 
-    static func validateSolution(config: Challenge, result: Solution, serializedInput: String) async throws -> Bool {
+    public static func validateSolution(config: Challenge, result: Solution, serializedInput: String) async throws -> Bool {
         return try await KerberusKt.validateSolution(config: config, result: result, serializedInput: serializedInput).boolValue
     }
 }
